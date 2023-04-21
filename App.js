c38a2c50b4b8535322af6ab7b7c0e51ee5c47d44
Tundekuzco <98494491+Tundekuzco@users.noreply.github.com>
2023-04-20 19:49:13 -0800

    Update App.js

--- App.js
+++ App.js
@@ -6,4 +6,29 @@ document.getElementById('create-project').addEventListener('submit', (e) => {
     console.log('Duration:', document.getElementById('duration').value);
 });
 
-document.getElementById('project-details').addEventListener('
+document.getElementById('project-details').addEventListener('project-details').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Call the viewProject function from your blockchain integration (e.g., using Web3.js)
+    console.log('Viewing project with the following ID:', document.getElementById('projectId').value);
+
+    // Replace the example data below with the data fetched from the smart contract
+    const exampleProjectData = {
+        title: 'Example Project',
+        description: 'This is an example project for demonstration purposes.',
+        fundingGoal: '10 ETH',
+        deadline: '2023-05-16',
+    };
+
+    document.getElementById('project-title').innerText = exampleProjectData.title;
+    document.getElementById('project-description').innerText = exampleProjectData.description;
+    document.getElementById('project-fundingGoal').innerText = `Funding Goal: ${exampleProjectData.fundingGoal}`;
+    document.getElementById('project-deadline').innerText = `Deadline: ${exampleProjectData.deadline}`;
+    document.getElementById('project-info').hidden = false;
+});
+
+document.getElementById('project-info').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Call the contribute function from your blockchain integration (e.g., using Web3.js)
+    console.log('Contributing to project with the following amount:');
+    console.log('Amount:', document.getElementById('contributionAmount').value);
+});
